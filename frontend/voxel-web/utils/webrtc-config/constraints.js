64564100d35eb9062fca0f5908dd54910242d3e1
Suryansh Mishra <suryanshmishra.0904@gmail.com>
2024-01-08 06:42:12 +0530

    Tried adding turn server

--- frontend/voxel-web/utils/webrtc-config/constraints.js
+++ frontend/voxel-web/utils/webrtc-config/constraints.js
@@ -1,5 +1,12 @@
 export const servers = {
-  iceServers: [{ urls: ['stun:stun1.l.google.com:19302'] }],
+  iceServers: [
+    { urls: ['stun:stun1.l.google.com:19302'] },
+    {
+      url: 'turn:numb.viagenie.ca',
+      credential: 'muazkh',
+      username: 'webrtc@live.com',
+    },
+  ],
 };
 
 export const mediaConstraints = { video: true, audio: true };
