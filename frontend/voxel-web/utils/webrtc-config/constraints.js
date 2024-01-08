8863f64dccb8ab43def85f9d2507a0eb672a9f31
Suryansh Mishra <suryanshmishra.0904@gmail.com>
2024-01-08 21:32:05 +0530

    Fiddling around with TURN

--- frontend/voxel-web/utils/webrtc-config/constraints.js
+++ frontend/voxel-web/utils/webrtc-config/constraints.js
@@ -1,23 +1,25 @@
 export const getIceServers = () => {
   const iceServers = [
-    { urls: 'stun:stun.relay.metered.ca:80' },
     {
-      urls: 'turn:standard.relay.metered.ca:80',
+      urls: 'stun:stun.relay.metered.ca:80',
+    },
+    {
+      urls: 'turn:global.relay.metered.ca:80',
       username: '7fc8c025d4bb0e41c86114b4',
       credential: 'ayA5rzDQn2OFfWVB',
     },
     {
-      urls: 'turn:standard.relay.metered.ca:80?transport=tcp',
+      urls: 'turn:global.relay.metered.ca:80?transport=tcp',
       username: '7fc8c025d4bb0e41c86114b4',
       credential: 'ayA5rzDQn2OFfWVB',
     },
     {
-      urls: 'turn:standard.relay.metered.ca:443',
+      urls: 'turn:global.relay.metered.ca:443',
       username: '7fc8c025d4bb0e41c86114b4',
       credential: 'ayA5rzDQn2OFfWVB',
     },
     {
-      urls: 'turn:standard.relay.metered.ca:443?transport=tcp',
+      urls: 'turn:global.relay.metered.ca:443?transport=tcp',
       username: '7fc8c025d4bb0e41c86114b4',
       credential: 'ayA5rzDQn2OFfWVB',
     },
